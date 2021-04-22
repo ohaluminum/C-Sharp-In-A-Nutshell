c8b8bb8102a0b9536ea6da3c28bd006b4aeb29d2
ohaluminum <lejinghuang2000@gmail.com>
2021-04-22 12:16:45 -0500

    Chapter 1 - 4: Enumeration and Iterators

---
+++ Program4/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Program4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // An array is already an IEnumerable => foreach works
+            var array = new int[] { 1, 2, 3 };
+
+            foreach (var a in array)
+            {
+                Console.WriteLine($"A is {a}");
+            }
+
+            // The foreach loop will be compile down to this version (low-level)
+            var enumerator = array.GetEnumerator();
+            
+            while (enumerator.MoveNext())    // Return true if there is another item   
+            {
+                Console.WriteLine($"Enumerator A is {enumerator.Current}");
+            }
+
+            // ------------------------ CUSTOM ENUMERABLE --------------------------------
+
+            GradeBook gradebook = new GradeBook();
+            gradebook.AddStudent(new Student() { FirstName = "Lejing", LastName = "Huang", UHID = 1800000, Grade = 85 });
+            gradebook.AddStudent(new Student() { FirstName = "Tony", LastName = "Stark", UHID = 1800001, Grade = 60.5m });
+            gradebook.AddStudent(new Student() { FirstName = "Winnie", LastName = "Li", UHID = 1800002, Grade = 90 });
+            gradebook.AddStudent(new Student() { FirstName = "Simon", LastName = "Shaw", UHID = 1800003, Grade = 10 });
+            
+            foreach (Student student in gradebook)
+            {
+                Console.WriteLine($"Name: {student.FirstName} {student.LastName}, UHID: {student.UHID}, Grade: {student.Grade}");
+            }
+        }
+
+        public class Student
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int UHID { get; set; }
+            public decimal Grade { get; set; }
+        }
+
+        public class GradeBook : IEnumerable<Student>    //Strengthen static type safety
+        {
+            // List of students
+            private IList<Student> studentList;
+
+            public GradeBook()
+            {
+                studentList = new List<Student>();
+            }
+
+            public void AddStudent(Student student)
+            {
+                studentList.Add(student);
+            }
+
+            // Generic enumerator 
+            public IEnumerator<Student> GetEnumerator()
+            {
+                foreach (Student student in studentList)
+                {
+                    yield return student;
+                }
+            }
+
+            // Non-generic enumerator: explicit implementation keeps it hidden: call the generic version
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+
+            /*
+             * NOTE:
+             * - This interface has two methods to implement and they are both called GetEnumerator. 
+             * You only need to worry about one of them. 
+             * The second is is there from the old non-generic IEnumerable interface. 
+             * You can safely call the generic version of the GetEnumerator method from the non-generic one.
+             */
+        }
+    }
+}
---
+++ Program5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // --------------------------------- .NET STRING AND TEXT HANDLING -----------------------------------
+
+            // Some static methods
+            Console.WriteLine(char.ToUpper('c', CultureInfo.InvariantCulture));    // Closely matches American culture.
+            Console.WriteLine(char.ToUpper('c', CultureInfo.CurrentCulture));    // Based on settings from the my computer’s control panel.
+            
+            Console.WriteLine(char.IsLetter('c'));    // True
+            Console.WriteLine(char.IsDigit('c'));    // False
+            Console.WriteLine(char.IsWhiteSpace('\t'));     // True
+
+            // Contruct a string
+
+            //@: To indicate that a string literal is to be interpreted verbatim.
+            //Reference: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/verbatim
+            //The following string looks better than "D:\\Program\\CSharp\\C-Sharp-In-A-Nutshell\\Program5\\Program.cs"
+            string path = @"D:\Program\CSharp\C-Sharp-In-A-Nutshell\Program5\Program.cs";
+            Console.WriteLine(path);
+
+            // Null and empty strings: string is reference type and can be null 
+            string emptyString = "";
+            Console.WriteLine(emptyString == "");              // True
+            Console.WriteLine(emptyString == string.Empty);    // True
+            Console.WriteLine(emptyString.Length == 0);        // True
+
+            string nullString = null;
+            Console.WriteLine(nullString == null);        // True
+            Console.WriteLine(nullString == "");          // False
+            //Console.WriteLine(nullString.Length == 0);    // NullReferenceException
+
+            // Iterating strings: string implements IEnumerable<char> interface
+            foreach (char c in "Hello, world!")
+            {
+                Console.Write(c + " ");    
+            }
+
+            Console.WriteLine();
+
+            // Manipulating strings: return a new string (string is immutable)
+            string s1 = "helloworld".Insert(5, ", ");    
+            Console.WriteLine(s1);      // s1 = "hello, world"
+
+            string s2 = s1.Remove(5, 2);                 
+            Console.WriteLine(s2);      // s2 = "helloworld"
+
+            string s3 = s1.Replace(", ", " | ");
+            Console.WriteLine(s3);
+
+
+
+
+        }
+    }
+}
